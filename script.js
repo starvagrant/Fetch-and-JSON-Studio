88635bd5f9b4b202fd9ed9a78450bc26d07117f5
James Ginns <starvagrant@yahoo.com>
2019-12-04 18:54:11 -0600

    Add HTML template for how the inner HTML might look like.
    Write the template that includes all the information needed to
    create the astronaut html from JSON.
    
    Notes: One will need to use this template seven times to get all
    astronauts. One potential issue is how template literals turn arrays
    to strings. The template might need to be modified if this is not
    the case.

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
        // Access the JSON in the response
        response.json().then( function(json) {
-          console.log(json);
+          let template = `
+            <div class="astronaut">
+                <div class="bio">
+                    <h3>${''}</h3>
+                    <ul>
+                        <li>Hours in space: ${''}</li>
+                        <li>Active: ${''}</li>
+                        <li>Skills: ${''}</li>
+                    </ul>
+                </div>
+                    <img class="avatar" src=${''}>
+                </div>
+            `;
        });
     });
  });
