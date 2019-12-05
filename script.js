75917c45ce1f471e7121c9fae9f3ce84652bb01a
James Ginns <starvagrant@yahoo.com>
2019-12-04 19:09:17 -0600

    Assignment displays correctly

--- script.js
+++ script.js
@@ -2,19 +2,24 @@ window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
        // Access the JSON in the response
        response.json().then( function(json) {
-          let template = `
+           let astronautHTML = "";
+           for (i=0;i<json.length;i++) {
+                astronautHTML += `
             <div class="astronaut">
                 <div class="bio">
-                    <h3>${''}</h3>
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
                     <ul>
-                        <li>Hours in space: ${''}</li>
-                        <li>Active: ${''}</li>
-                        <li>Skills: ${''}</li>
+                        <li>Hours in space: ${json[i].hoursInSpace}</li>
+                        <li>Active: ${json[i].active}</li>
+                        <li>Skills: ${json[i].skills}</li>
                     </ul>
                 </div>
-                    <img class="avatar" src=${''}>
+                    <img class="avatar" src=${json[i].picture}>
                 </div>
             `;
+           }
+           astronautContainer = document.getElementById('container');
+           astronautContainer.innerHTML = astronautHTML;
        });
     });
  });
