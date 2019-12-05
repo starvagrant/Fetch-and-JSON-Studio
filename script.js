7870f02a99402488884c27ac8f63244a6026d5f5
James Ginns <starvagrant@yahoo.com>
2019-12-04 18:48:55 -0600

    Correctly call json and log data to console

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
+       // Access the JSON in the response
+       response.json().then( function(json) {
+          console.log(json);
+       });
+    });
+ });
